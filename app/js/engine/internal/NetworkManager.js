c17a0c290141be6bd2860969b2bfbb36f4ef98ef
Joshua Schwarz <josh@amagit.com>
2014-02-09 00:06:20 +0000

    Additional work on NetworkManager.

--- app/js/engine/internal/NetworkManager.js
+++ app/js/engine/internal/NetworkManager.js
@@ -12,51 +12,82 @@ goog.provide('CrunchJS.Internal.NetworkManager');
  */
 CrunchJS.Internal.NetworkManager = function() {
 	/**
+	 * API key for PeerJS-Server
+	 * @type {string}
+	 */
+	this.apiKey = 'i45z4s9chvv7k3xr';
+
+	/**
 	 * The string ID of this peer
 	 * @type {string}
 	 */
 	this.peerId = '';
 	
 	/**
+	 * Peer object
+	 * @type {Object}
+	 */
+	this.peer = null;
+	
+	/**
 	 * Peers to which this peer is connected
 	 * @type {Array}
-	 * @protected
 	 */
 	this.connectedPeers = [];
+	
+	/**
+	 * Connection objects for connected peers
+	 * @type {Array}
+	 */
+	 this.connections = [];
+	 
+	 this.initialize();
 };
 
-/**
- * Checks if a peer is in the connected peers list
- * @return {boolean} True or false
- * @param {string} pId A peer ID
- */
-CrunchJS.Internal.NetworkManager.prototype.isConnected = function(pId)
+CrunchJS.Internal.NetworkManager.prototype.initialize = function()
 {
-	for(var k in this.connectedPeers)
-	{
-		if(k == pId)
-		{
-			return true;
-		}
-	}
-	return false;
-};
+	var peer = new Peer({key: apiKey});
+	//on a connection
+	peer.on('open', function(id) {
+		//record the generated ID
+		this.peerId = id;
+	});
+	peer.on('connection', function(conn) {
+		//define data handling function
+		conn.on('data', function(data){
+			if(data['type'] == 'connect')
+			{
+				//If somebody connected to me then I should connect back (if necessary).
+				var otherPeer = data['data'];
+				if(this.connectedPeers.indexOf(otherPeer) == -1)
+				{
+					this.connect(peer, otherPeer);
+					console.log("I am reciprocating a conn with: " + otherPeer);
+				}
+			}
+			else
+			{
+				console.log("GOT DATA: " + data);
+			}
+		});
+	});
+	this.peer = peer;
+}
 
 /**
- * Connects to a new peer
- * @param {string} pId A peer ID
+ * Connect to a new peer
+ * @param {string} pId A remote peer ID
  */
 CrunchJS.Internal.NetworkManager.prototype.connect = function(pId)
 {
-	
+	var conn = this.peer.connect(pId);
+	//open a connection
+	conn.on('open', function(){
+		//Tell the peer that you want to connect with him.
+		conn.send({'type':'connect','data':this.peerId});
+		//Save the connection.
+		connected.push(pId);
+		connections.push(conn);
+	});
 };
 
-/**
- * Inserts peer ID into connected peers list
- * @param {string} pId The peer ID of a newly connected peer
- * @param {Object} peerConnection The peerJS object for the connection
- */
-CrunchJS.Internal.NetworkManager.prototype.addPeer = function(pId, peerConnection)
-{
-	this.connectedPeers[pId] = peerConnection;
-};
