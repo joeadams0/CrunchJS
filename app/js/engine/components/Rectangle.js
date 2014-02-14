d322143e45f13d88714cb05492acb90bf56c37bd
Justin White <jrw152@case.edu>
2014-02-13 20:31:44 -0500

    added density, friction and restitution to Rectangle.js

--- app/js/engine/components/Rectangle.js
+++ app/js/engine/components/Rectangle.js
@@ -4,8 +4,7 @@
 
 goog.provide('CrunchJS.Components.Rectangle');
 
-goog.require('CrunchJS.Component');
-
+goog.require('CrunchJS.Component'); 
 
 
 /**
@@ -14,11 +13,14 @@ goog.require('CrunchJS.Component');
  * @param {Object} vertex2 Defines the location of one point of a rectangle with a x, y vector
  * @param {Object} vertex3 Defines the location of one point of a rectangle with a x, y vector
  * @param {Object} vertex4 Defines the location of one point of a rectangle with a x, y vector
+ * @param {number} density Density of the entity
+ * @param {number} friction Friction of the entity
+ * @param {number} restitution Restitution of the entity
  * @constructor
  * @class 
  * @extends {CrunchJS.Component}
  */
-CrunchJS.Components.Rectangle = function(vertex1, vertex2, vertex3, vertex4) {
+CrunchJS.Components.Rectangle = function(vertex1, vertex2, vertex3, vertex4, density, friction, restitution) {
 
 	/**
 	 * The velocity of the entity	
@@ -55,6 +57,24 @@ CrunchJS.Components.Rectangle = function(vertex1, vertex2, vertex3, vertex4) {
 		x : 0,
 		y : 0
 	};
+
+	/**
+	 * Density of the entity
+	 * @type {number}
+	 */
+	this.density = density;
+
+	/**
+	 * Friction of the entity
+	 * @type {number}
+	 */
+	this.friction = friction;
+
+	/**
+	 * Restitution of the entity
+	 * @type {number}
+	 */
+	this.restitution = restitution;
 };
 
 goog.inherits(CrunchJS.Components.Rectangle, CrunchJS.Component);
